5bf6c7809dc0f93282af3aea3d5813a579a54e5b
Dmytro Sushchenko <34779566+OBess@users.noreply.github.com>
2022-10-26 14:42:08 +0300

    Added GoogleTests lib

---
+++ tests/tests.cpp
@@ -0,0 +1,27 @@
+#include <vector>
+
+#include <gtest/gtest.h>
+
+#include "../SeidelsMethod.h"
+
+TEST(SeidelsMethod, n_2_003f)
+{
+    const std::vector<float_t> expected {0};
+
+    sfe::SeidelsMethod seidelsMethod(2, 0.003f);
+    seidelsMethod.compute();
+
+    EXPECT_EQ(expected.size(), seidelsMethod.result().size());
+
+    for (size_t i = 0; i < expected.size(); ++i)
+    {
+        EXPECT_FLOAT_EQ(expected[i], seidelsMethod.result()[i]);
+    }
+}
+
+
+int main(int argc, char* argv[])
+{
+    testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}

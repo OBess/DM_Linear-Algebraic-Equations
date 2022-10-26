340005b9e78e3da77564f3077129b76c0438faf6
Dmytro Sushchenko <34779566+OBess@users.noreply.github.com>
2022-10-26 14:54:59 +0300

    Reorganized files/folders

---
+++ resources/languages/DM_Linear-Algebraic-Equations_uk_UA.ts
@@ -0,0 +1,143 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="uk_UA">
+<context>
+    <name>LinearAlgebraicEquations</name>
+    <message>
+        <location filename="../../views/linearalgebraicequations.ui" line="26"/>
+        <location filename="../../build/DM_Linear-Algebraic-Equations_autogen/include/ui_linearalgebraicequations.h" line="67"/>
+        <location filename="../../build/DM_Linear-Algebraic-Equations_autogen/ui_linearalgebraicequations.h" line="67"/>
+        <location filename="../../build/DM_Linear-Algebraic-Equations_autogen/views/ui_linearalgebraicequations.h" line="67"/>
+        <source>СЛАУ Методом Зейделя</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../views/linearalgebraicequations.ui" line="39"/>
+        <location filename="../../build/DM_Linear-Algebraic-Equations_autogen/include/ui_linearalgebraicequations.h" line="68"/>
+        <location filename="../../build/DM_Linear-Algebraic-Equations_autogen/ui_linearalgebraicequations.h" line="68"/>
+        <location filename="../../build/DM_Linear-Algebraic-Equations_autogen/views/ui_linearalgebraicequations.h" line="68"/>
+        <source>Кількість змінних</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../views/linearalgebraicequations.ui" line="56"/>
+        <location filename="../../build/DM_Linear-Algebraic-Equations_autogen/include/ui_linearalgebraicequations.h" line="69"/>
+        <location filename="../../build/DM_Linear-Algebraic-Equations_autogen/ui_linearalgebraicequations.h" line="69"/>
+        <location filename="../../build/DM_Linear-Algebraic-Equations_autogen/views/ui_linearalgebraicequations.h" line="69"/>
+        <source>2</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../views/linearalgebraicequations.ui" line="61"/>
+        <location filename="../../build/DM_Linear-Algebraic-Equations_autogen/include/ui_linearalgebraicequations.h" line="70"/>
+        <location filename="../../build/DM_Linear-Algebraic-Equations_autogen/ui_linearalgebraicequations.h" line="70"/>
+        <location filename="../../build/DM_Linear-Algebraic-Equations_autogen/views/ui_linearalgebraicequations.h" line="70"/>
+        <source>3</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../views/linearalgebraicequations.ui" line="66"/>
+        <location filename="../../build/DM_Linear-Algebraic-Equations_autogen/include/ui_linearalgebraicequations.h" line="71"/>
+        <location filename="../../build/DM_Linear-Algebraic-Equations_autogen/ui_linearalgebraicequations.h" line="71"/>
+        <location filename="../../build/DM_Linear-Algebraic-Equations_autogen/views/ui_linearalgebraicequations.h" line="71"/>
+        <source>4</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../views/linearalgebraicequations.ui" line="71"/>
+        <location filename="../../build/DM_Linear-Algebraic-Equations_autogen/include/ui_linearalgebraicequations.h" line="72"/>
+        <location filename="../../build/DM_Linear-Algebraic-Equations_autogen/ui_linearalgebraicequations.h" line="72"/>
+        <location filename="../../build/DM_Linear-Algebraic-Equations_autogen/views/ui_linearalgebraicequations.h" line="72"/>
+        <source>5</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../views/linearalgebraicequations.ui" line="76"/>
+        <location filename="../../build/DM_Linear-Algebraic-Equations_autogen/include/ui_linearalgebraicequations.h" line="73"/>
+        <location filename="../../build/DM_Linear-Algebraic-Equations_autogen/ui_linearalgebraicequations.h" line="73"/>
+        <location filename="../../build/DM_Linear-Algebraic-Equations_autogen/views/ui_linearalgebraicequations.h" line="73"/>
+        <source>6</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../views/linearalgebraicequations.ui" line="81"/>
+        <location filename="../../build/DM_Linear-Algebraic-Equations_autogen/include/ui_linearalgebraicequations.h" line="74"/>
+        <location filename="../../build/DM_Linear-Algebraic-Equations_autogen/ui_linearalgebraicequations.h" line="74"/>
+        <location filename="../../build/DM_Linear-Algebraic-Equations_autogen/views/ui_linearalgebraicequations.h" line="74"/>
+        <source>7</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../views/linearalgebraicequations.ui" line="86"/>
+        <location filename="../../build/DM_Linear-Algebraic-Equations_autogen/include/ui_linearalgebraicequations.h" line="75"/>
+        <location filename="../../build/DM_Linear-Algebraic-Equations_autogen/ui_linearalgebraicequations.h" line="75"/>
+        <location filename="../../build/DM_Linear-Algebraic-Equations_autogen/views/ui_linearalgebraicequations.h" line="75"/>
+        <source>8</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../views/linearalgebraicequations.ui" line="100"/>
+        <location filename="../../build/DM_Linear-Algebraic-Equations_autogen/include/ui_linearalgebraicequations.h" line="77"/>
+        <location filename="../../build/DM_Linear-Algebraic-Equations_autogen/ui_linearalgebraicequations.h" line="77"/>
+        <location filename="../../build/DM_Linear-Algebraic-Equations_autogen/views/ui_linearalgebraicequations.h" line="77"/>
+        <source>Далі</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>UISedelsResult</name>
+    <message>
+        <location filename="../../views/uisedelsresult.ui" line="14"/>
+        <location filename="../../build/DM_Linear-Algebraic-Equations_autogen/include/ui_uisedelsresult.h" line="45"/>
+        <location filename="../../build/DM_Linear-Algebraic-Equations_autogen/ui_uisedelsresult.h" line="45"/>
+        <location filename="../../build/DM_Linear-Algebraic-Equations_autogen/views/ui_uisedelsresult.h" line="45"/>
+        <source>Результати розв&apos;язку СЛАУ</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>UISeidelsSetup</name>
+    <message>
+        <location filename="../../views/uiseidelssetup.ui" line="26"/>
+        <location filename="../../build/DM_Linear-Algebraic-Equations_autogen/include/ui_uiseidelssetup.h" line="79"/>
+        <location filename="../../build/DM_Linear-Algebraic-Equations_autogen/ui_uiseidelssetup.h" line="79"/>
+        <location filename="../../build/DM_Linear-Algebraic-Equations_autogen/views/ui_uiseidelssetup.h" line="79"/>
+        <source>Встановлення значень</source>
+        <oldsource>Задання значень</oldsource>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../views/uiseidelssetup.ui" line="39"/>
+        <location filename="../../build/DM_Linear-Algebraic-Equations_autogen/include/ui_uiseidelssetup.h" line="80"/>
+        <location filename="../../build/DM_Linear-Algebraic-Equations_autogen/ui_uiseidelssetup.h" line="80"/>
+        <location filename="../../build/DM_Linear-Algebraic-Equations_autogen/views/ui_uiseidelssetup.h" line="80"/>
+        <source>Кількість змінних: 3</source>
+        <oldsource>Кількість змінних</oldsource>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../views/uiseidelssetup.ui" line="52"/>
+        <location filename="../../build/DM_Linear-Algebraic-Equations_autogen/include/ui_uiseidelssetup.h" line="81"/>
+        <location filename="../../build/DM_Linear-Algebraic-Equations_autogen/ui_uiseidelssetup.h" line="81"/>
+        <location filename="../../build/DM_Linear-Algebraic-Equations_autogen/views/ui_uiseidelssetup.h" line="81"/>
+        <source>Точність ε = </source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../views/uiseidelssetup.ui" line="102"/>
+        <location filename="../../build/DM_Linear-Algebraic-Equations_autogen/include/ui_uiseidelssetup.h" line="82"/>
+        <location filename="../../build/DM_Linear-Algebraic-Equations_autogen/ui_uiseidelssetup.h" line="82"/>
+        <location filename="../../build/DM_Linear-Algebraic-Equations_autogen/views/ui_uiseidelssetup.h" line="82"/>
+        <source>Вирішити</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../views/uiseidelssetup.ui" line="115"/>
+        <location filename="../../build/DM_Linear-Algebraic-Equations_autogen/include/ui_uiseidelssetup.h" line="83"/>
+        <location filename="../../build/DM_Linear-Algebraic-Equations_autogen/ui_uiseidelssetup.h" line="83"/>
+        <location filename="../../build/DM_Linear-Algebraic-Equations_autogen/views/ui_uiseidelssetup.h" line="83"/>
+        <source>Скасувати</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
